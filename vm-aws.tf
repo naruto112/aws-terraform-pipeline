8ee09d213b7f7d2350a89fc559619892e7b786a6
Renato Souza <renatorock3@hotmail.com>
2023-06-18 18:19:38 -0300

    v5 - domingo

--- vm-aws.tf
+++ vm-aws.tf
@@ -9,8 +9,8 @@ resource "aws_instance" "vm" {
   instance_type = "t2.micro"
   # key_name                    = aws_key_pair.key.key_name
 #   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
-#   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
-  associate_public_ip_address = true
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address   = true
 
   tags = {
     "Name" = "vm-terraform"
