d69500c4454ee38bb513643b34981af17c60ba58
Renato Souza <renatorock3@hotmail.com>
2023-06-18 16:58:37 -0300

    v4 - hoje

--- vm-aws.tf
+++ vm-aws.tf
@@ -3,16 +3,16 @@
 #   public_key = var.aws_pub_key
 # }
 
-# resource "aws_instance" "vm" {
-#   depends_on    = [aws_vpc.vpc, aws_subnet.subnet]
-#   ami           = "ami-022e1a32d3f742bd8"
-#   instance_type = "t2.micro"
-#   # key_name                    = aws_key_pair.key.key_name
+resource "aws_instance" "vm" {
+  depends_on    = [aws_vpc.vpc, aws_subnet.subnet]
+  ami           = "ami-022e1a32d3f742bd8"
+  instance_type = "t2.micro"
+  # key_name                    = aws_key_pair.key.key_name
 #   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
 #   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
-#   associate_public_ip_address = true
+  associate_public_ip_address = true
 
-#   tags = {
-#     "Name" = "vm-terraform"
-#   }
-# }
+  tags = {
+    "Name" = "vm-terraform"
+  }
+}
